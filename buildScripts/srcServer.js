11bbb6023b5591c031cf257a5446c225dbffe001
Jacek Leoniec <jacekleoniec@Jaceks-iMac.local>
2017-01-01 21:56:56 +0000

    Added Initial HTTP Mocking notes and some sample code

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -23,6 +23,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName": "Smith","email":"info@info.com"},
+    {"id": 2,"firstName":"Bob2","lastName": "Smith2","email":"info2@info.com"},
+    {"id": 3,"firstName":"Bob3","lastName": "Smith3","email":"info3@info.com"},
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
